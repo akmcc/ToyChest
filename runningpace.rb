1e65d9a927986ec802c2c083757b89feac9b7947
Ashton McCrate <a.mccrate@gmail.com>
2013-07-30 20:26:28 -0700

    add array to store last five paces

--- runningpace.rb
+++ runningpace.rb
@@ -1,24 +1,46 @@
 
-def findPace (t, d)
-	@pace = (t/d).round(2)
+def findPace (time, distance)
+  return (time/distance).round(2)
 end
-  
-puts "How many miles did you run today?"
-d = gets.chomp.to_f
-puts "How many minutes did it take you to complete this run?"
-puts "(ex: 120.75 minutes = 2 hrs 45 seconds)"
-t = gets.chomp.to_f
 
-findPace(t, d)
-
-puts "You ran at a #{@pace} minute/mile pace."
-
-if @pace <= 8  #faster than 8
-	puts "Well done!"
-elsif @pace > 8 && @pace <= 9.5 #between 8 and 9.5
-	puts "Let's pick up the pace next time..."
-elsif @pace > 9.5 && @pace <= 11 #between 9.5 and 11
-	puts "What? Surely you ran faster than that, right?"
-else #anything slower than 11
-	puts "You weren't running. You must've been meandering."
+def getRunInfo
+	puts "How many miles did you run?"
+	distance = gets.chomp.to_f
+	puts "How many minutes did it take you to complete this run?"
+	puts "(ex: 120.75 minutes = 2 hrs 45 seconds)"
+	time = gets.chomp.to_f
+	return findPace(time, distance)
 end
+
+@last_five_paces = []
+
+def runner
+	pace = getRunInfo
+
+	puts "You ran at a #{pace} minute/mile pace."
+	if pace <= 8.5  
+		puts "Well done!"
+	else #anything slower than 8.5
+		puts "Let's pick up the pace next time..."
+	end
+
+	if @last_five_paces.size == 5
+		@last_five_paces.shift
+	end
+	@last_five_paces<<pace
+
+	puts "Would you like to log another run [y/n]?"
+	another_run = gets.chomp
+
+	if another_run == "y"
+		runner 
+	elsif another_run == "n"
+		sum_of_paces = @last_five_paces.inject(0.0) {|sum, pace| sum + pace}
+	  average_pace = (sum_of_paces/@last_five_paces.size).round(2)
+	  puts "The average pace of your most recent (up to five) runs is #{average_pace} minutes per mile."
+	end     
+end
+
+runner
+
+
