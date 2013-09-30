792b4cdf045e3a8706df1eb2521d945bf8641aac
Ashton McCrate <a.mccrate@gmail.com>
2013-09-30 12:50:24 -0400

    fix teens and allow accurate translations to 999,999

--- spellnumbers.rb
+++ spellnumbers.rb
@@ -2,6 +2,10 @@ def spell_number(number)
 
 string_number = ""
 
+if number == 0 then 
+    string_number = "zero"
+end
+
 write = number/1000
 
 case write
@@ -15,6 +19,12 @@ when 7 then string_number = (string_number + " seven thousand")
 when 8 then string_number = (string_number + " eight thousand")
 when 9 then string_number = (string_number + " nine thousand")
 end
+
+if write > 9 
+    spell_number(write) 
+    print " thousand"
+end
+
 
 remainder = number - write*1000
 
@@ -37,7 +47,20 @@ remainder = remainder - write*100
 write = remainder/10
 
 case write
-when 1 then string_number = (string_number + " this part gets tricky")
+when 1 then 
+    if remainder == 10 then string_number = (string_number + " ten")
+    elsif remainder == 11 then string_number = (string_number + " eleven")
+    elsif remainder == 12 then string_number = (string_number + " twelve")
+    elsif remainder == 13 then string_number = (string_number + " thirteen")
+    elsif remainder == 14 then string_number = (string_number + " fourteen")
+    elsif remainder == 15 then string_number = (string_number + " fifteen")
+    elsif remainder == 16 then string_number = (string_number + " sixteen")
+    elsif remainder == 17 then string_number = (string_number + " seventeen")
+    elsif remainder == 18 then string_number = (string_number + " eighteen")
+    elsif remainder == 19 then string_number = (string_number + " nineteen")
+    end
+    puts string_number
+    abort
 when 2 then string_number = (string_number + " twenty")
 when 3 then string_number = (string_number + " thirty")
 when 4 then string_number = (string_number + " fourty")
@@ -65,11 +88,12 @@ when 9 then string_number = (string_number + " nine")
 end
 
 
-puts string_number
+print string_number
 
 end
 
-puts "Give me a number between 0 and 9,999:"
+puts "Give me a number between 0 and 999,999:"
 input = gets.chomp.to_i
 spell_number(input)
 
+puts
