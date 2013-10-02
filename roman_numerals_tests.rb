e3f8b9ab0fba7fac58672d27f3c492f6690604d4
Ashton McCrate <a.mccrate@gmail.com>
2013-10-02 18:09:46 -0400

    renamed roman numbers file

--- roman_numerals_tests.rb
+++
@@ -1,9 +0,0 @@
-require addition_only_roman_numerals
-
-def tester(arg, expected_answer)
-  actual_answer = romanize(arg)
-  if actual_answer != expected_answer
-    puts "test failed"
-    puts actual_answer, "not equal to ", expected_answer
-  end
-end
