a7eabc8c22aef0b0392ae6ede0105e25ae906312
Ashton McCrate <a.mccrate@gmail.com>
2013-10-02 16:48:37 -0400

    refactor all the things

--- addition_only_roman_numerals.rb
+++ addition_only_roman_numerals.rb
@@ -10,46 +10,43 @@ end
 def romanize(number)
  @roman_numeral="" 
   
-  if number == 0
-    return "please give me a number greater than zero"
-  end
-  
-  hundred = { 4 => "DC", 
+if number == 0
+  return "please give me a number greater than zero"
+end
+digits = {
+
+  :thousands => {
+        "num" => 1000,
+        1 => "M"},
+
+  :hundred => { 
+        "num" => 100,
+        4 => "DC", 
         9 => "CM",
         5 => "D",
-        1 => "C" }
+        1 => "C" },
 
-ten = {  4 => "XL",
+:ten => {  
+        "num" => 10,
+        4 => "XL",
         9 => "XC",
         5 => "L",
-        1 => "X" }
+        1 => "X" },
 
-one = {   4 => "IV",
+:one => {   
+        "num" => 1,
+        4 => "IV",
         9 => "IX",
         5 => "V",
-        1 => "I" }
-  
-  remainder = number
-  @write = remainder/1000 #find out how many thousands
-  remainder = remainder - @write*1000
-  if @write > 0
-    @roman_numeral += ("M" * @write)
-  end
- 
-  @write = remainder/100 #find out how many hundreds 
-  remainder = remainder - @write*100
-    translate_for(hundred)
+        1 => "I" }}
 
+remainder = number
 
-  @write = remainder/10 #find out how many tens
-  remainder = remainder - @write*10
-
-  translate_for(ten)
-
-
-  @write = remainder #only ones left
-
-  translate_for(one)
+digits.each do |key, value|
+  @write = remainder/value["num"] 
+  remainder = remainder - @write*value["num"]
+  translate_for(value)
+end
 
 @roman_numeral
 end
@@ -57,18 +54,18 @@ end
 def translate_for(arg)
 
 if @write > 0
-    if @write == 4
-      @roman_numeral += arg[4] 
-    elsif @write == 9
-      @roman_numeral += arg[9]
-    elsif @write >= 5
-      @roman_numeral += arg[5]
-      @write = @write - 5
-      @roman_numeral += (arg[1] * @write)
-    else 
-      @roman_numeral += (arg[1] * @write)
-    end
+  if @write == 4
+    @roman_numeral += arg[4] 
+  elsif @write == 9
+    @roman_numeral += arg[9]
+  elsif @write >= 5
+    @roman_numeral += arg[5]
+    @write = @write - 5
+    @roman_numeral += (arg[1] * @write)
+  else 
+    @roman_numeral += (arg[1] * @write)
   end
+end
 end
 
 def tester(arg, expected_answer)
@@ -84,7 +81,6 @@ end
 end
 
 number = Roman_numbers.new
-
 
 number.tester(999, "CMXCIX")
 number.tester(99, "XCIX")
