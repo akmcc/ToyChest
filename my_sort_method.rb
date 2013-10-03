a140d9a00cf237b84d06d603ddba1676eea71996
Ashton McCrate <a.mccrate@gmail.com>
2013-10-03 16:21:19 -0400

    works with duplicate values in a list

--- my_sort_method.rb
+++ my_sort_method.rb
@@ -1,6 +1,5 @@
 require 'colorize'
 
-
 class Sorter
   def initialize
     @unsorted_list = []
@@ -14,7 +13,7 @@ class Sorter
     end
 
     @sorted_list.unshift(@unsorted_list.max)
-    @unsorted_list.delete(@unsorted_list.max)
+    @unsorted_list.delete_at(@unsorted_list.index(@unsorted_list.max))
     
     if @unsorted_list.size == 1
       @sorted_list.unshift(@unsorted_list[0])
@@ -22,10 +21,8 @@ class Sorter
       my_sort(@unsorted_list)
     end
 
-   @sorted_list 
+    @sorted_list 
   end
-
-
 
   def tester(*things_to_sort)
     real_sort= *things_to_sort.sort
@@ -42,7 +39,6 @@ class Sorter
   end
 
 end
-
 
 sort_list = Sorter.new
 sort_list.tester(3, 6, 1, 9, 5, 3, 4)
