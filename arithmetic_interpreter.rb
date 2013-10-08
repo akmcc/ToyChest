5ffa869cd09af504c1e8549751d0336a42d36eb5
Ashton McCrate <a.mccrate@gmail.com>
2013-10-08 18:43:27 -0400

    super wet but calculates multiples, no OoOps yet

--- arithmetic_interpreter.rb
+++ arithmetic_interpreter.rb
@@ -1,28 +1,40 @@
-def calculate(string)
-  characters = string.split(" ")
-  characters = characters.map do |char|
-                 if char.to_i != 0
-                    char.to_i
-                 else
-                    char = char
-                 end
-               end
+class Calculator
+  def find_characters(string)
+    @characters = string.split(" ")
+    @characters = @characters.map do |char|
+      if char.to_i != 0
+        char.to_i
+      else
+        char = char
+      end
+    end
+  end
 
-print characters
-puts
-  num1 = characters[0]
-  operator = characters[1]
-  num2 = characters[2]
+  def eval
+    if @characters.include?("*")
+      value = (@characters[(@characters.index("*"))-1]) * (@characters[(@characters.index("*"))+1])
+      @characters[((@characters.index("*"))-1)..((@characters.index("*"))+1)] = [value]
+     
+    elsif @characters.include?("+")
+      value = (@characters[(@characters.index("+"))-1]) + (@characters[(@characters.index("+"))+1])
+      @characters[((@characters.index("+"))-1)..((@characters.index("+"))+1)] = [value]
+     
+    elsif @characters.include?("-")
+      value = (@characters[(@characters.index("-"))-1]) - (@characters[(@characters.index("-"))+1])
+      @characters[((@characters.index("-"))-1)..((@characters.index("-"))+1)] = [value]  
+    end
+    if @characters.size > 1
+        eval
+    end
+  end
 
-
-case operator
-when "+" then puts num1 + num2
-when "*" then puts num1 * num2
-when "-" then puts num1 - num2
+  def calculate(string)
+    find_characters(string)
+    eval
+    @characters
+  end
 end
 
-end
-
-calculate("2 + 2")
-calculate("2 * 3")
-calculate("6 - 3")
+thing = Calculator.new
+puts thing.calculate("2 * 3 * 4")
+puts thing.calculate("2 + 3 + 6 - 1")
