d2a550fa3e386713d74818c7f6c83070f33fb9b9
Ashton McCrate <a.mccrate@gmail.com>
2013-11-06 18:09:14 -0500

    restrategize

--- tcptoy.rb
+++ tcptoy.rb
@@ -1,110 +1,38 @@
-require 'pry'
+# examples of packets:
+# {type: "handshake", reply_to: "sender"}
+# {type: "data", data: "blah blah message blah", id: 1}
+# {type: "receipt", id: 1}
+# other potential packets:
+# {type: "data_start", data_stop_id: 35}
+# OR
+# {type: "data_start"}
+# {type: "data_stop"}
 
-#this model assumes server is already in listening mode
+class Communicator
 
-#server
-class Sender 
+  attr_accessor :partner
+  received_data = []
 
-  attr_accessor :receipts
-
-  def initialize
-    @handshake = false
-    @tcp_segments = {}
-    @receipts = []
-    @message = "I saw my life branching out before me like the green fig tree in the story. From the tip of every branch, like a fat purple fig, a wonderful future beckoned and winked. One f"
-    
-  end
-
-  def handshake
-    @handshake = true
-  end
-
-  def create_tcp_segments
-    packets = @message.split_into_packets_of(8) 
-    packets.each_with_index do |packet, index|
-      @tcp_segments[index] = packet
-    end
-    @tcp_segments
-  end
-
-  def write_to(destination)
-    create_tcp_segments
-    if @handshake
-      (Hash[@tcp_segments.to_a.reverse]).each_pair do |header, body|
-        until is_acknowledged?(header)
-          destination.read_buffer[header] = body
-          sleep(0.2)
-          destination.send_read_receipt_to(self) #not the senders job to send the read_receipt, need to move to recevier class
-        end
-      end
+  def in(packet)
+    if packet[type] == handshake
+      #make sender your connection partner and send handshake packet back to sender
+    elsif packet[type] == data
+      received_data << packet
+    elsif packet[type] == receipt
+      #do something else, not sure what yet
     end
   end
 
-  def is_acknowledged?(header)
-    @receipts.include?(header)
-  end
+  def out(packet)
 
-end
-
-#client
-class Receiver 
-
-  attr_accessor :read_buffer, :read_receptacle
-
-  def initialize
-    @handshake = false
-  end
-
-  def establish_connection_with(server)
-    @handshake = true
-    server.handshake #is it okay to have methods calling methods on instances of another class?
-    @read_buffer = {}
-    @read_receptacle = {}
-  end
-
-  def send_read_receipt_to(sender)
-    if @handshake
-      header = @read_buffer.first[0]
-      body = @read_buffer.first[1]
-      @read_receptacle[header] = body
-      sender.receipts << header
-      @read_buffer.shift
-    end
-  end
-
-  def reorder_data
-    @read_receptacle = Hash[@read_receptacle.sort]
-  end   
-
-  def read 
-    reorder_data
-    message = ""
-    @read_receptacle.each_pair do |header, body|
-      message += body
-    end
-    message
-  end                 
-
-end
-
-module TCPPackets
-  def split_into_packets_of(num)
-    packets = []
-    until self.length == 0
-      packets << self.slice!(0,num)
-    end
-    packets
   end
 end
 
-class String
-  include TCPPackets
-end
+sender = Communicator.new
+receiver = Communicator.new
 
-sender = Sender.new
-receiver = Receiver.new
-receiver.establish_connection_with(sender)
-sender.write_to(receiver)
-pry
-#receiver.read 
+
+
+
+
 
