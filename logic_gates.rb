64640dd833e98eba420126a0cee6c7f8e7eaf1db
Ashton McCrate <a.mccrate@gmail.com>
2013-12-05 14:24:38 -0500

    add multiplexor

--- logic_gates.rb
+++ logic_gates.rb
@@ -25,33 +25,52 @@ end
 
 class And < Struct.new(:left, :right)
   def run
-    out = Nand.new(left, right).run
-    Not.new(out).run
+    nand = Nand.new(left, right).run
+    Not.new(nand).run
   end
 end
 
 class Or < Struct.new(:left, :right)
   def run
-    out1 = Nand.new(left, left).run
-    out2 = Nand.new(right, right).run
-    Nand.new(out1, out2).run
+    nand1 = Nand.new(left, left).run
+    nand2 = Nand.new(right, right).run
+    Nand.new(nand1, nand2).run
   end
 end
 
 class Nor < Struct.new(:left, :right)
   def run
-    out = Or.new(left, right).run
-    Not.new(out).run
+    or1 = Or.new(left, right).run
+    Not.new(or1).run
   end
 end
 
 class Xor < Struct.new(:left, :right)
   def run
-    out1 = Not.new(left).run
-    out2 = And.new(out1, right).run
-    out3 = Not.new(right).run
-    out4 = And.new(left, out3).run
-    Or.new(out2, out4).run
+    not_left = Not.new(left).run
+    and1 = And.new(not_left, right).run
+    not_right = Not.new(right).run
+    and2 = And.new(left, not_right).run
+    Or.new(and1, and2).run
+  end
+end
+
+class Multiplexor < Struct.new(:left, :right, :selector)
+  def run
+    not_right = Not.new(right).run
+    and1 = And.new(left, not_right).run
+    not_selector = Not.new(selector).run
+    and2 = And.new(and1, not_selector).run
+    and3 = And.new(left, right).run
+    and4 = And.new(not_selector, and3).run
+    not_left = Not.new(left).run
+    and5 = And.new(not_left, right).run
+    and6 = And.new(and5, selector).run
+    and7 = And.new(left, right).run
+    and8 = And.new(and7, selector).run
+    or1 = Or.new(and6, and8).run
+    or2 = Or.new(and2, and4).run
+    Or.new(or1, or2).run
   end
 end
 
@@ -183,5 +202,50 @@ describe 'Xor' do
     result = Xor.new(Binary.new(1),Binary.new(1)).run
     result.must_equal Binary.new(0)
   end 
+end
 
+describe 'Multiplexor' do 
+
+  # 000 means left is zero right is zero and selector is zero
+  # 101 means left is one right is zero and selector is one 
+ 
+  it "should return 0 with 000" do
+    result = Multiplexor.new(Binary.new(0), Binary.new(0), Binary.new(0)).run
+    result.must_equal Binary.new(0)
+  end
+
+  it "should return 0 with 010" do
+    result = Multiplexor.new(Binary.new(0), Binary.new(1), Binary.new(0)).run
+    result.must_equal Binary.new(0)
+  end
+
+  it "should return 1 with 100" do
+    result = Multiplexor.new(Binary.new(1), Binary.new(1), Binary.new(0)).run
+    result.must_equal Binary.new(1)
+  end
+
+  it "should return 1 with 110" do
+    result = Multiplexor.new(Binary.new(1), Binary.new(1), Binary.new(0)).run
+    result.must_equal Binary.new(1)
+  end
+
+  it "should return 0 with 001" do
+    result = Multiplexor.new(Binary.new(0), Binary.new(0), Binary.new(1)).run
+    result.must_equal Binary.new(0)
+  end
+
+  it "should return 1 with 011" do
+    result = Multiplexor.new(Binary.new(0), Binary.new(1), Binary.new(1)).run
+    result.must_equal Binary.new(1)
+  end
+
+  it "should return 0 with 101" do
+    result = Multiplexor.new(Binary.new(1), Binary.new(0), Binary.new(1)).run
+    result.must_equal Binary.new(0)
+  end
+
+  it "should return 1 with 111" do
+    result = Multiplexor.new(Binary.new(1), Binary.new(1), Binary.new(1)).run
+    result.must_equal Binary.new(1)
+  end
 end
